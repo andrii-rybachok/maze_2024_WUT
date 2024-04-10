f932896646a6594016384d8b0c5549b7282c8524
Chaienko Alexandr <153275738+SkyForce06@users.noreply.github.com>
2024-04-10 15:05:27 +0200

    Update operacja.h

--- src/operacja.h
+++ src/operacja.h
@@ -1,43 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <string.h>
+#include <stdint.h> 
 
-
-#define FILE_ID 0x52524243
-#define ESCAPE_CHAR 0x1B
 
 typedef struct {
-    uint32_t fileId;
+    uint32_t file_id;
     uint8_t escape;
     uint16_t columns;
     uint16_t lines;
-    uint16_t entryX;
-    uint16_t entryY;
-    uint16_t exitX;
-    uint16_t exitY;
-    uint32_t reserved;
+    uint16_t entry_x;
+    uint16_t entry_y;
+    uint16_t exit_x;
+    uint16_t exit_y;
     uint32_t counter;
-    uint32_t solutionOffset;
+    uint32_t solution_offset;
     uint8_t separator;
     uint8_t wall;
     uint8_t path;
-} BinaryMazeHeader;
-
-typedef struct {
-    uint8_t separator;
-    uint8_t value;
-    uint8_t count;
-} BinaryWordHeader;
+} FileHeader;
 
 typedef struct {
     uint32_t direction;
     uint8_t steps;
-} BinarySolutionHeader;
+} SolutionHeader;
 
 typedef struct {
     uint8_t direction;
     uint8_t counter;
-} BinarySolutionStep;
+} SolutionStep;
 
