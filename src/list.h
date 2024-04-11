72050cb5d48ad0b5f596f70423a4d8b1202b0e5c
Chaienko Alexandr <153275738+SkyForce06@users.noreply.github.com>
2024-04-12 01:02:30 +0200

    dodania zapisania do plika binarnego"

--- src/list.h
+++ src/list.h
@@ -1,23 +1,21 @@
 
+
+#include "mazeParams.h"
+#include "binaryAdapter.h"
+
+#pragma pack(push, 1)
 typedef struct  node{
-    unsigned short countOfSteps: 14;
-    unsigned short direction : 2;
+    unsigned short countOfSteps: 14; // liczba kroków
+    unsigned short direction : 2; // 0 - lewo, 1 - prawo, 2 - dół, 3 - góra 
     struct node* next;
 } node;
-typedef struct cords
-{
-    int x;
-    int y;
-} cords;
-typedef struct listParams
-{
-    int* visitedVertices;
-    cords * currentCords;
-    int lastVerticeNumber;
-} listParams;
+#pragma pack(pop)
 
-void show(node *head);
-void push_back(node **head, int steps,int direction);
-void pop_back(node **head);
-int getListSteps(node *head);
-void printSolution(char* fileName, node *head);
+
+
+
+
+void push_back(node **head, int steps,int direction); // dodanie do końca listy
+void pop_back(node **head); // usuwanie z końca listy
+int getListSteps(node *head); // suma wszystkich kroków
+void saveSolution(node *head,mazeParams params); // zapisywanie rozwiązania binarnego lub tekstowego
