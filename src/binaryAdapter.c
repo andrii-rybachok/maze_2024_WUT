77e3add43ed151b9577dde54e89bab19dc2c2c6a
Chaienko Alexandr <153275738+SkyForce06@users.noreply.github.com>
2024-04-12 01:00:28 +0200

    Funkcja dla pliku binarnego i plik nagłówkowy

---
+++ src/binaryAdapter.c
@@ -0,0 +1,44 @@
+#include "binaryAdapter.h"
+
+binHeader getFileHeader(char* fileName){
+    FILE *file = fopen(fileName, "rb");
+    if (file == NULL) {
+        handleError(2);
+        return;
+    }
+    binHeader header;
+    fread(&header, sizeof(header), 1, file);
+    return header;
+}
+
+void printBinSolution(char* fileName){
+    FILE *file = fopen(fileName, "rb");
+    if (file == NULL) {
+        perror("Error opening file");
+        return;
+    }
+    solutionHeader header;
+    fread(&header, sizeof(header), 1, file);
+    printf("Solution ID %d, kroki %d \n",header.solutionId,header.steps);
+    solutionStep step;
+
+    while (fread(&step, sizeof(step), 1, file) == 1) {
+        printf("Kierunek: %c, kroki: %d\n", step.direction, step.counter);
+    }
+}
+void printBinHeader(const binHeader *header) {
+    printf("File ID: %u\n", header->file_id);
+    printf("Escape: %c\n", header->escape);
+    printf("Columns: %hu\n", header->columns);
+    printf("Lines: %hu\n", header->lines);
+    printf("Entry X: %hu\n", header->entry_x);
+    printf("Entry Y: %hu\n", header->entry_y);
+    printf("Exit X: %hu\n", header->exit_x);
+    printf("Exit Y: %hu\n", header->exit_y);
+    printf("Reserved: %Lf\n", header->reserved);
+    printf("Counter: %d\n", header->counter);
+    printf("Solution Offset: %d\n", header->solution_offset);
+    printf("Separator: %c\n", header->separator);
+    printf("Wall: %c\n", header->wall);
+    printf("Path: %c\n", header->path);
+}
