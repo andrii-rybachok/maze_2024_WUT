07557da916751c530bd0db07924aeeb8402b099a
Chaienko Alexandr <153275738+SkyForce06@users.noreply.github.com>
2024-04-12 01:06:07 +0200

    Update maze.h

--- src/maze.h
+++ src/maze.h
@@ -1,46 +1,33 @@
+
+#include "userCommunication.h"
 #include "list.h"
-#include "operacja.h" 
+#include "binaryAdapter.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MAZE_START 'P'
-#define MAZE_END 'K'
-#define MAZE_WALL 'X'
-#define MAZE_PATH ' '
+
+#define MAZE_START 'P' // Symbol początku
+#define MAZE_END 'K' //końca
+#define MAZE_WALL 'X'// ściany
+#define MAZE_PATH ' '//ścieżki
 
 
-#define MAZE_SIZE_X 2049
+#define MAZE_SIZE_X 2049 //maksymalne rozmiary labiryntu
 #define MAZE_SIZE_Y 2049
 
 
 
+void findSolution(unsigned int maze[],mazeParams* params); // znałezienie oraz zapisywanie najkr. ścieżki
 
-typedef struct mazeParams
-{
-    int cols;
-    int rows;
-    char* originFileName;
-    char* ourFileName;
-    cords endCords;
-    cords startCords;
-    int minSteps;
-} mazeParams;
+mazeParams initializeParams(char* originFileName); // inicjalizacja parametrów dla labiryntu (txt)
+mazeParams initializeParamsBin(char* originFileName); // inicjalizacja parametrów dla labiryntu (bin)
+
+int readMazeFromFile(unsigned int maze[], mazeParams params); // wyczytanie labiryntu z pliku txt
+void readMazeFromBinFile(unsigned int maze[],mazeParams params); // wyczytanie labiryntu z pliku bin
+
+void removeDeadEnds(unsigned int maze[],mazeParams params); // usuwanie ślepych zaułków
 
 
-int getCountOfVertices(char * fileName);
 
-
-void findSolution(unsigned int maze[],mazeParams* params);
-
-mazeParams initializeParams(char* originFileName,char* ourFileName);
-
-void writeMazeToFile(unsigned int maze[],mazeParams params);
-
-int readMazeFromFile(unsigned int maze[], mazeParams params);
-
-void removeDeadEnds(unsigned int maze[],mazeParams params);
-
-
-void convertBinaryToText(const char *binaryFilename, const char *textFilename);
